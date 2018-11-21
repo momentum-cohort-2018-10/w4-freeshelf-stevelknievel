c61b22f52deda681a83c360ce41b8c1ed9ac90c5
stevelknievel <marks.steven@gmail.com>
2018-11-20 21:03:53 -0500

    migrated database.  manually added 10 or so books to have seed data.  added class: Category to class: Book.  More done.

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from collection.models import Book
+# import model above
 
-# Register your models here.
+
+# create automated slug creation:
+class BookAdmin(admin.ModelAdmin):
+    model = Book
+    list_display = ('title', 'author', 'description',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+# register below:
+admin.site.register(Book, BookAdmin)
--- collection/models.py
+++ collection/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     description = models.TextField()
-    url = models.Charfield()
+    url = models.URLField()
     slug = models.SlugField(unique=True)
-    # date_added = models.DateTime??   need to figure this one out
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_last_updated = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
 
